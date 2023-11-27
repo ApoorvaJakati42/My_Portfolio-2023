f38d3022334c10b3a601cc5bf1e46e0d9eca57b1
Apoorva <apoorva.jakati.react@gmail.com>
2023-11-27 14:39:47 +0530

    Added contents to about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Hero from './components/hero/hero';
+import About from './components/about/about'
 
 function App() {
   return (
     <div className="App">
       <link rel="stylesheet" href="./css/icon-font.css" />
       <Hero/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/about/about.js
@@ -0,0 +1,55 @@
+import './about.scss';
+import '../hero/hero.scss'
+import nat1Large from '../../img/nat-1-large.jpg';
+import nat2Large from '../../img/nat-2-large.jpg';
+import nat3Large from '../../img/nat-3-large.jpg';
+import react from '../../img/react-new.jpg';
+import layout from '../../img/layout.jpg';
+import digital from '../../img/digital.jpg';
+
+
+function About() {
+    return (
+      <>
+       <section class="section-about">
+            <div class="u-center-text u-marginBottom-big">
+                    <h2 class="heading-secondary">
+                        About MySelf
+                    </h2>
+            </div>
+
+            <div class="row">
+                <div class="col-1-of-2">
+                    <h3 class="heading-tertiary u-marginBottom-small"> Personal Details</h3>
+                    <p class="my-details">
+                       Name : Mr. Apoorva A Jakati <br/>
+                       Date Of Birth : 29/07/1993 <br/>
+                       Email : apoorva.jakati@gmail.com <br/>
+                       Languages : English , Kannada
+                            
+                    </p>
+
+                    <h3 class="heading-tertiary u-marginBottom-small">Professsional Summary</h3>
+                    <p class="paragraph">
+                    Software Developer with 2 years & 10 months of experience in software development using
+                    Java and Javascript technologies. Proficient with tools such as Eclipse and VS Code.Passionate about coding.
+                            
+                    </p>
+
+                    <a  href="#" class="btn-text">Learn More &rarr;</a>
+                </div>
+                
+                <div class="col-1-of-2">
+                    <div class="composition">
+                        <img src={react} alt="Photo 1" class="composition__photo composition__photo--p1"/>
+                        <img src={layout} alt="Photo 2" class="composition__photo composition__photo--p2"/>
+                        <img src={digital} alt="Photo 3" class="composition__photo composition__photo--p3"/>
+                    </div>
+                </div>
+            </div>
+       </section>
+      </>
+    );
+  }
+  
+  export default About;
